1d48e4af7bb4aa01c12f7fe4361a43b9478d5de1
Carolina Knoll <carolinasknoll@gmail.com>
2017-07-01 15:20:42 -0300

    Remove unneeded info

--- js/scripts.js
+++ js/scripts.js
@@ -14,46 +14,6 @@ $(function () {
   });
 });
 
-// Daily Workflow Chart
-var ctx = document.getElementById('workflowChart');
-var workflowChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: {
-    labels: ['Working', 'Studying', 'Eating', 'Trying new things', 'Watching videos/anime', 'Resting/Fun', 'Sleeping'],
-    datasets: [{
-      data: [6, 2, 2, 2, 1, 2, 9],
-      backgroundColor: [
-        '#a5dff9',
-        '#88dba3',
-        '#fff1b9',
-        '#feee7d',
-        '#23A8AD',
-        '#9DC3C1'
-      ],
-      borderColor: [
-        '#FFF',
-        '#FFF',
-        '#FFF',
-        '#FFF',
-        '#FFF',
-        '#FFF',
-        '#FFF'
-      ],
-      borderWidth: 4
-    }],
-  },
-  options: {
-    responsive: true,
-    maintainAspectRatio: true,
-    animation: {
-      animateRotate: true,
-    },
-    legend: {
-      display: true,
-    }
-  }
-});
-
 //Growth Chart
 var ctx2 = document.getElementById('growthChart');
 var growthChart = new Chart(ctx2, {
