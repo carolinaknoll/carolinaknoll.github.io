2454bd7f8173e8c32d799feffd62ed9c66924dd6
Carolina Seleme Knoll <carolinasknoll@gmail.com>
2021-03-23 10:48:20 -0300

    improv(colors): use color variables for colors

--- src/config/css-variables.js
+++ src/config/css-variables.js
@@ -6,11 +6,19 @@ styles['default-print-font-family'] = "'EB Garamond', sans-serif"
 
 styles['white'] = '#fff'
 styles['white-2'] = 'rgba(200, 200, 200, 0.8)'
+styles['white-3'] = '#dce2e6'
+styles['white-4'] = '#cfd8dc'
+styles['white-5'] = '#e6e6e6'
 
 styles['dark'] = '#000'
+styles['dark-2'] = '#1d2129'
+styles['dark-3'] = '#15181e'
 
 styles['blue-1'] = '#2bbbff'
 styles['blue-2'] = '#156f99'
+styles['blue-3'] = 'rgba(0, 194, 255, 0.5)'
+styles['blue-4'] = 'rgba(0, 194, 255, 0.7)'
+styles['blue-5'] = '#d6fffc'
 
 styles['purple-1'] = '#c28cff'
 styles['purple-2'] = '#5f2d91'
@@ -18,11 +26,13 @@ styles['purple-3'] = '#c58cff'
 
 styles['green-1'] = '#76f7a5'
 styles['green-2'] = '#3f915d'
+styles['green-3'] = '#1e8257'
 
 styles['orange-1'] = '#ba8b02'
 styles['orange-2'] = '#f0db9e'
 
 styles['gray-1'] = '#181818'
 styles['gray-2'] = 'rgba(40, 40, 40, 0.8)'
+styles['gray-3'] = '#9e9e9e'
 
 module.exports = styles
