53fdfd1ae762de7c27f52299aa29d779ac9592d8
Carolina Seleme Knoll <carolinasknoll@gmail.com>
2022-06-23 20:30:14 -0300

    improv: remove obsolete component and rework home sentence

--- src/app/components/templates/home/hero/index.tsx
+++ src/app/components/templates/home/hero/index.tsx
@@ -10,30 +10,20 @@ import styles from './styles.module.scss';
 const HeroSection = () => (
   <section className={styles.home} id="home">
     <div className={styles.background}>
-      <div className={styles.homeDescriptionWrapper}>
-        <div className={styles.homeDescription}>
-          <div className={styles.titleContainer}>
-            <Flower className={styles.titleImage} />
-
-            <Heading type={HeadingLevels.h2} form={HeadingFonts.print}>
-              <FormattedMessage id="homeDescription1" />
-            </Heading>
-            <Heading
-              type={HeadingLevels.h2}
-              form={HeadingFonts.cursive}
-              className={styles.homeTitle}
-            >
-              <FormattedMessage id="homeDescription2" />
-            </Heading>
-          </div>
-
-          <Heading type={HeadingLevels.h6} form={HeadingFonts.print} className={styles.foo}>
-            <FormattedMessage id="homeDescription3" />{' '}
-            <a href="https://en.wikipedia.org/wiki/Virtue" target="_blank">
-              <FormattedMessage id="homeDescription4" />
-            </a>
+      <div className={styles.homeDescription}>
+        <div className={styles.titleContainer}>
+          <Flower className={styles.titleImage} />
+          <Heading type={HeadingLevels.h2} form={HeadingFonts.cursive} className={styles.homeTitle}>
+            <FormattedMessage id="homeDescription1" />
           </Heading>
         </div>
+
+        <Heading type={HeadingLevels.h6} form={HeadingFonts.print} className={styles.foo}>
+          <FormattedMessage id="homeDescription2" />{' '}
+          <a href="https://en.wikipedia.org/wiki/Virtue" target="_blank">
+            <FormattedMessage id="homeDescription3" />
+          </a>
+        </Heading>
       </div>
     </div>
   </section>
